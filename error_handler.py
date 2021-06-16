2a69b7bfb4adc5f94b87ccb26e52947cc54962fa
Arie Roos <ariechildofgod@gmail.com>
2021-06-16 19:00:57 +0200

    Add handle error method

--- error_handler.py
+++ error_handler.py
@@ -1,2 +1,8 @@
-def handle_exception(err: Exception):
+def handle_exception(err: Exception, raise_again=False):
     print(f"exception ({type(err)}) occurred: {err}")
+    if raise_again:
+        raise err
+
+
+def handle_error(msg: str):
+    print(f"Error occurred: {msg}")
