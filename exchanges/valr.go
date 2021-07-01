1f9d43c29bad2208ffe38c5031bcd0fb5fcba4cd
Arie Roos <ariechildofgod@gmail.com>
2021-07-01 21:46:20 +0200

    Expand exchange interface

--- exchanges/valr.go
+++ exchanges/valr.go
@@ -1,6 +1,39 @@
 package exchanges
 
-type Valr struct {
+import "github.com/arieroos/crypto-trader/interfaces"
+
+type Valr struct{}
+
+func (v Valr) Balance(currency string) (float64, error) {
+	panic("implement me")
+}
+
+func (v Valr) OrderSummary(orderID string) (interfaces.OrderSummary, error) {
+	panic("implement me")
+}
+
+func (v Valr) ListOpenOrders() ([]interfaces.OrderSummary, error) {
+	panic("implement me")
+}
+
+func (v Valr) CancelOrder(orderID string) error {
+	panic("implement me")
+}
+
+func (v Valr) BuyAtMarket(input interfaces.OrderInput) (averagePrice float64, err error) {
+	panic("implement me")
+}
+
+func (v Valr) SellAtMarket(input interfaces.OrderInput) (averagePrice float64, err error) {
+	panic("implement me")
+}
+
+func (v Valr) PlaceBuyOrder(input interfaces.OrderInput) (orderID string, err error) {
+	panic("implement me")
+}
+
+func (v Valr) PlaceSellOrder(input interfaces.OrderInput) (orderID string, err error) {
+	panic("implement me")
 }
 
 func (v Valr) LastTradedPrice(pair string) (float64, error) {
--- interfaces/interfaces.go
+++ interfaces/interfaces.go
@@ -2,4 +2,36 @@ package interfaces
 
 type ExchangeAPI interface {
 	LastTradedPrice(pair string) (float64, error)
+	Balance(currency string) (float64, error)
+	OrderSummary(orderID string) (OrderSummary, error)
+	ListOpenOrders() ([]OrderSummary, error)
+	CancelOrder(orderID string) error
+	// BuyAtMarket buys the base currency at whatever price is available on the market,
+	// using the base amount if it is not 0 or quote amount if base amount is 0.
+	BuyAtMarket(input OrderInput) (averagePrice float64, err error)
+	// SellAtMarket sells the base currency at whatever price is available on the market,
+	// using the base amount if it is not 0 or quote amount if base amount is 0.
+	SellAtMarket(input OrderInput) (averagePrice float64, err error)
+	PlaceBuyOrder(input OrderInput) (orderID string, err error)
+	PlaceSellOrder(input OrderInput) (orderID string, err error)
+}
+
+type Side string
+
+const (
+	SideBuy  Side = "buy"
+	SideSell Side = "sell"
+)
+
+type OrderSummary struct {
+	OrderID       string
+	Side          Side
+	Success       bool
+	FailureReason string
+}
+
+type OrderInput struct {
+	Pair        string
+	BaseAmount  float64
+	QuoteAmount float64
 }
